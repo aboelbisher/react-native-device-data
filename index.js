6d0f050a3ed8ed404774193ffeee7a483fabeac7
Muhammad Abed El Razek <muhammadr@wixs-MacBook-Pro.local>
2019-01-03 13:43:26 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,6 @@
+
+import { NativeModules } from 'react-native';
+
+const { RNReactNativeDeviceData } = NativeModules;
+
+export default RNReactNativeDeviceData;
---
+++ ios/RNReactNativeDeviceData.m
@@ -0,0 +1,15 @@
+
+#import "RNReactNativeDeviceData.h"
+
+@implementation RNReactNativeDeviceData
+
+- (dispatch_queue_t)methodQueue
+{
+    return dispatch_get_main_queue();
+}
+RCT_EXPORT_MODULE()
+
+
+
+@end
+  
